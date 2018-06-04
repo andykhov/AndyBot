93ffec7d1f9d080c98f956a79af49805c722f781
Andy Khov <khovandyak@gmail.com>
2018-06-03 22:05:15 -0700

    added end comments

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,8 +6,8 @@ public class Main {
             System.err.println("ERROR: Please include bot token as first argument");
         }
 
-        IDiscordClient client = BotUtils.buildDiscordClient(args[0]);
+        IDiscordClient client = BotUtils.buildDiscordClient(args[0]); //builds client with token
         client.getDispatcher().registerListener(new EventHandler());
-        client.login();
+        client.login(); //login to invited guilds
     }
 }
