b06e0dcb268c7ca631c86a676d3e0868555175cf
Andy Khov <khovandyak@gmail.com>
2018-06-04 11:54:07 -0700

    implemented audio player manager

--- src/main/java/EventHandler.java
+++ src/main/java/EventHandler.java
@@ -3,9 +3,26 @@ import sx.blah.discord.handle.impl.events.guild.channel.message.MessageReceivedE
 import sx.blah.discord.handle.obj.IUser;
 import sx.blah.discord.handle.obj.IGuild;
 import sx.blah.discord.handle.obj.IRole;
+
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
+import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager;
+import com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+
 import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
 
 public class EventHandler {
+    private final AudioPlayerManager = playerManager;
+    private final Map<Long, GuildMusicManager> musicManagers; // coordinates GuildMusicManagers for each guild
+
+    public EventHandler() {
+        this.musicManagers = new HashMap<>();
+        this.playerManager = new DefaultAudioPlayerManager();
+        AudioSourceManagers.registerRemoteSources(playerManager);
+        AudioSourceManagers.registerLocalSource(playerManager);
+    }
 
     @EventSubscriber
     public void onMessageReceived(MessageReceivedEvent event) {
@@ -20,6 +37,26 @@ public class EventHandler {
             }
         }
     }
+
+    private synchronized GuildMusicManager getGuildAudioPlayer(IGuild guild) {
+        GuildMusicManager musicManager = musicManagers.get(guild.getLongId());
+
+        if (musicManager == null) {
+            musicManager = new GuildMusicManager();
+            musicManagers.put(guild.getLongId(), musicManager);
+        }
+
+        guild.getAudioManager().setAudioProvider(musicManager);
+
+        return musicManager;
+    }
+
+    private void play(IGuild guild, GuildMusicManager musicManager, AudioTrack track) {
+        GuildMusicManager manager = getGuildAudioPlayer(guild);
+        manager.trackHandler.queue(track);
+    }
+
+
 
     //returns true if user has the top role in the guild
     private boolean hasTopRole(IGuild guild, IUser user) {
