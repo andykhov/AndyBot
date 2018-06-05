fb8bb85f7a5c8b27313908eba5a9d704db2d6f78
Andy Khov <khovandyak@gmail.com>
2018-06-05 12:20:04 -0700

    fixed compile errors

--- src/main/java/EventHandler.java
+++ src/main/java/EventHandler.java
@@ -1,20 +1,27 @@
-import sx.blah.discord.api.events.EventSubscriber;
-import sx.blah.discord.handle.impl.events.guild.channel.message.MessageReceivedEvent;
 import sx.blah.discord.handle.obj.IUser;
 import sx.blah.discord.handle.obj.IGuild;
 import sx.blah.discord.handle.obj.IRole;
+import sx.blah.discord.handle.obj.IChannel;
+import sx.blah.discord.handle.obj.IVoiceChannel;
+import sx.blah.discord.handle.audio.IAudioManager;
+import sx.blah.discord.api.events.EventSubscriber;
+import sx.blah.discord.handle.impl.events.guild.channel.message.MessageReceivedEvent;
+import sx.blah.discord.util.MissingPermissionsException;
 
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
 import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager;
 import com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers;
+import com.sedmelluq.discord.lavaplayer.track.AudioPlaylist;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+import com.sedmelluq.discord.lavaplayer.player.AudioLoadResultHandler;
+import com.sedmelluq.discord.lavaplayer.tools.FriendlyException;
 
 import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
 
 public class EventHandler {
-    private final AudioPlayerManager = playerManager;
+    private final AudioPlayerManager playerManager;
     private final Map<Long, GuildMusicManager> musicManagers; // coordinates GuildMusicManagers for each guild
 
     public EventHandler() {
@@ -41,23 +48,52 @@ public class EventHandler {
         }
     }
 
+    private void loadAndPlay(IChannel channel, String url) {
+        GuildMusicManager manager = getGuildAudioPlayer(channel.getGuild());
+        /*
+        playerManager.loadItemOrdered(manager, url, new AudioLoadResultHandler() {
+            @Override
+            public void trackLoaded(AudioTrack track) {
+                BotUtils.sendMessage(channel, "Adding to queue "  + track.getInfo().title); 
+            }
+        });*/
+    }
+
     private synchronized GuildMusicManager getGuildAudioPlayer(IGuild guild) {
-        GuildMusicManager musicManager = musicManagers.get(guild.getLongId());
+        GuildMusicManager musicManager = musicManagers.get(guild.getLongID());
 
         if (musicManager == null) {
-            musicManager = new GuildMusicManager();
-            musicManagers.put(guild.getLongId(), musicManager);
+            musicManager = new GuildMusicManager(playerManager);
+            musicManagers.put(guild.getLongID(), musicManager);
         }
 
-        guild.getAudioManager().setAudioProvider(musicManager);
+        guild.getAudioManager().setAudioProvider(musicManager.getAudioProvider());
 
         return musicManager;
     }
 
-    private void play(IGuild guild, GuildMusicManager musicManager, AudioTrack track) {
-        GuildMusicManager manager = getGuildAudioPlayer(guild);
+    /* queue given AudioTrack to guild's track handler */
+    private void play(IGuild guild, GuildMusicManager manager, AudioTrack track) {
+        connectToFirstVoiceChannel(guild.getAudioManager());
         manager.trackHandler.queue(track);
     }
+
+    private static void connectToFirstVoiceChannel(IAudioManager audioManager) {
+        for (IVoiceChannel voiceChannel : audioManager.getGuild().getVoiceChannels()) {
+            if (voiceChannel.isConnected()) {
+                return;
+            }
+        }
+
+        for (IVoiceChannel voiceChannel : audioManager.getGuild().getVoiceChannels()) {
+            try {
+                voiceChannel.join();
+            } catch (MissingPermissionsException e) {
+                // log.warn("Cannot enter voice channel {}", voiceChannel.getName(), e);
+            }
+        }
+    }
+
 
 
 
